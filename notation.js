af5c15df0d28895301a831f25369edc89a7ce47a
Alex Henderson <bohroco@gmail.com>
2020-08-20 14:54:07 -0500

    Update notation.js

--- notation.js
+++ notation.js
@@ -1,17 +1,19 @@
 function format(value) {
-const log8 = Math.LN10 / LOG8 * value.log10();
+    const BARS = ["", "", "", "", "", "", "", ""];
+    const LOG8 = Math.log(8);
+    const log8 = Math.LN10 / LOG8 * value.log10();
     let wholeLog = Math.floor(log8);
     const decimalLog = log8 - wholeLog;
     const decimalLog64 = Math.floor(decimalLog * 64);
     const parts = [
-      BARS[decimalLog64 % 8],
-      BARS[Math.floor(decimalLog64 / 8)]
+        BARS[decimalLog64 % 8],
+        BARS[Math.floor(decimalLog64 / 8)]
     ];
     while (wholeLog >= 8) {
-      const remainder = wholeLog % 8;
-      wholeLog = (wholeLog - remainder) / 8;
-      parts.push(BARS[remainder]);
+        const remainder = wholeLog % 8;
+        wholeLog = (wholeLog - remainder) / 8;
+        parts.push(BARS[remainder]);
     }
     parts.push(BARS[wholeLog]);
     return parts.join("");
-    }
+}
