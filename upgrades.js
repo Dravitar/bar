df693c286187b5cea756c107d57c37a7cce5fbb0
Alex Henderson <bohroco@gmail.com>
2020-08-24 17:10:32 -0500

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -1,1 +1,12 @@
+function createUpgrade(id, amount, index, growth) {
+  var newUpgrade = document.createElement("button");
+  newUpgrade.id = id;
+  
+}
 
+function loadUpgradeSet(num) {
+  switch(num) {
+    case 1:
+      createUpgrade("barAutoPresser", 1, 2, false);
+  }
+}
