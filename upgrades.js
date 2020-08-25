34c77f5ae95d146faaef9bd269409cbde1b84600
Alex Henderson <bohroco@gmail.com>
2020-08-25 15:03:43 -0500

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -9,6 +9,8 @@ function Upgrade(id, price, index, repeated, growth, purchased) {
 
 let barAutoPresser = new Upgrade("barAutoPresser", 1, 2, false, 0, 0);
 
+let allUpgrades = [barAutoPresser];
+
 function createUpgrade(upgrade, type) {
   let symbol;
   switch(type) {
@@ -26,13 +28,17 @@ function createUpgrade(upgrade, type) {
     indexBars+= bar;
   }
   newUpgrade.id = upgrade.id;
-  newUpgrade.setAttribute("onclick", "checkUpgrade("+upgrade+")");
+  newUpgrade.setAttribute("onclick", "checkUpgrade("+upgrade.id+")");
   newUpgrade.innerHTML = "format("+upgrade.price+") "+indexBars+"<br/>"+
     symbol+indexBars;
   $("upgrades").appendChild(newUpgrade);
 }
 
-function checkUpgrade(upgrade) {
+function checkUpgrade(id) {
+  let upgrade;
+  allUpgrades.forEach(function(item) {
+    if(item.id==id) upgrade = item;
+  });
   if(upgrade.purchased<=upgrade.repeated&&user.bars[upgrade.index-1]>=upgrade.price){
     upgrade.purchased++;
     user.bars[upgrade.index-1]-=upgrade.price;
