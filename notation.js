9bf29e012e859a2586efb2fdb0b000e794ee9bff
Alex Henderson <bohroco@gmail.com>
2020-08-25 15:07:49 -0500

    Update notation.js

--- notation.js
+++ notation.js
@@ -1,7 +1,7 @@
 function format(value) {
     const BARS = ["", "", "", "", "", "", "", ""];
     const LOG8 = Math.log(8);
-    const log8 = Math.LN10 / LOG8 * value.log10();
+    const log8 = Math.LN10 / LOG8 * Math.log10(value);
     let wholeLog = Math.floor(log8);
     const decimalLog = log8 - wholeLog;
     const decimalLog64 = Math.floor(decimalLog * 64);
